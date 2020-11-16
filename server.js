3603e6280800b43c19bd7f42171d7b170f8fe0fa
christopher@zarlengo.net <christopher@zarlengo.net>
2020-11-15 18:48:35 -0800

    change dependencies

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const db = require('./models');
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
-
 }
 
 // Coding to json
