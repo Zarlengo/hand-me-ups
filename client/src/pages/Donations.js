c67b28a0973bf0cbae6dbc7bddf4e0beef83b519
Erin Frasier <e_marie84@ymail.com>
2020-12-02 19:54:35 -0800

    Merge branch 'master' of https://github.com/Zarlengo/hand-me-ups into styling

--- client/src/pages/Donations.js
+++ client/src/pages/Donations.js
@@ -5,9 +5,11 @@ import './donationStyle.css';
 
 function Donations() {
     return (
-        <div className="grid loggedInDiv">
-            <h1>Donations</h1>
-            <Outgoing />
+        <div className="container1">
+            <div className="grid loggedInDiv">
+                <h1>Donations</h1>
+                <Outgoing />
+            </div>
         </div>
     );
 }
